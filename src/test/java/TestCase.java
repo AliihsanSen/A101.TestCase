e9a0e03cfb80da8129417415a7f8bea6f358685a
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-01 20:55:51 +0300

    01.10.2022

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.WebElement;
 
 public class TestCase extends TestBase {
 
-     /*
-    Uçtan uca ödeme ekranına kadar Selenium’da java dili ile chrome browser kullanarak test otomasyon ödevi yapılacak.
-    Ödeme ekranı doldurulmayacak. Aşağıdaki senaryoyu web ve mobil olmak üzere 2 çeşit oluşturabilirlerse çok iyi olur. En az Web’de yapmak zorunlu.
-    - Senaryoya üye kaydı oluşturmadan devam edilecek.
-    - Giyim--> Aksesuar--> Kadın İç Giyim-->Dizaltı Çorap bölümüne girilir.
-    - Açılan ürünün siyah olduğu doğrulanır.
-    - Sepete ekle butonuna tıklanır.
-    - Sepeti Görüntüle butonuna tıklanır.
-    - Sepeti Onayla butonuna tıklanır.
-    - Üye olmadan devam et butonuna tıklanır.
-    - Mail ekranı gelir.
-    - Sonrasında adres ekranı gelir. Adres oluştur dedikten sonra ödeme ekranı gelir.
-    - Siparişi tamamla butonuna tıklayarak, ödeme ekranına gidildiği ,doğru ekrana yönlendiklerini kontrol edecekler.
+    /**
+     * Uçtan uca ödeme ekranına kadar Selenium’da java dili ile chrome browser kullanarak test otomasyon ödevi yapılacak.
+     * Ödeme ekranı doldurulmayacak. Aşağıdaki senaryoyu web ve mobil olmak üzere 2 çeşit oluşturabilirlerse çok iyi olur. En az Web’de yapmak zorunlu.
+     * - Senaryoya üye kaydı oluşturmadan devam edilecek.
+     * - Giyim--> Aksesuar--> Kadın İç Giyim-->Dizaltı Çorap bölümüne girilir.
+     * - Açılan ürünün siyah olduğu doğrulanır.
+     * - Sepete ekle butonuna tıklanır.
+     * - Sepeti Görüntüle butonuna tıklanır.
+     * - Sepeti Onayla butonuna tıklanır.
+     * - Üye olmadan devam et butonuna tıklanır.
+     * - Mail ekranı gelir.
+     * - Sonrasında adres ekranı gelir. Adres oluştur dedikten sonra ödeme ekranı gelir.
+     * - Siparişi tamamla butonuna tıklayarak, ödeme ekranına gidildiği ,doğru ekrana yönlendiklerini kontrol edecekler.
      */
 
     @Test
@@ -83,11 +83,11 @@ public class TestCase extends TestBase {
 
         // Kullanıcı il seçer.
         driver.findElement(By.xpath("//select[@name='city']")).sendKeys("KONYA");
-Thread.sleep(100);
+        Thread.sleep(100);
 
         // Kullanıcı ilçe seçer.
         driver.findElement(By.xpath("//select[@name='township']")).sendKeys("SELÇUKLU");
-Thread.sleep(100);
+        Thread.sleep(100);
 
         // Kullanıcı mahalle seçer.
         driver.findElement(By.xpath("//select[@class='js-district']")).
